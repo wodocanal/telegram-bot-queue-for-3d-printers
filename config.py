b185b081acffa309c3f12fa71b8a5aaef93b78bc
wodo <96520106+wodocanal@users.noreply.github.com>
2023-12-19 09:02:27 +0300

    Update config.py
    yes

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import time
 import os
 
-TOKEN = '6040239312:AAH-fFssTQKU3t-aFMB1j87KrlhlTrkT8Z8'
+TOKEN = 'YOUR_TOKEN'
 DOWNLOAD_DIR = 'C:\\Users\\mosvo\\Documents\\programming\\tg_bot_waitlist\\models\\'
 
 EXTNESIONS = ['.stl', '.stp', '.step', '.3mf', '.a3d', '.m3d', '.gcode']
